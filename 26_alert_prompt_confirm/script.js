b7cf5c6566f35291f3851a76bafb502c313f554a
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-25 12:27:19 +0000

    write to the document based on the confirm

--- 26_alert_prompt_confirm/script.js
+++ 26_alert_prompt_confirm/script.js
@@ -4,3 +4,9 @@ a = Number.parseInt(a);
 alert("you enterd a of type "+ (typeof a))
 
 let write = confirm("Do you want to write it to the page ?")
+if (write){
+  document.write(a)
+}
+else{
+  document.write("Please allow me to write")
+}
