3b1e52f3c92456e6bbd8958756d5eed63790b9dd
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-25 07:30:31 +0000

    check time in the console

--- 25_console_Objects/script.js
+++ 25_console_Objects/script.js
@@ -3,4 +3,12 @@ console.info("info");
 console.warn("warning");
 console.error('err');
 console.assert('err' != false);
-console.assert('err' == false)
+console.assert('err' == false);
+
+console.time('forLoop')
+
+for (let i = 0; i < 5; i++){
+  console.log(233)
+}
+
+console.timeEnd("forLoop")
