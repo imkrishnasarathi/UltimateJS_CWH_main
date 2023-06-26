1a82581b044b101e17ed97322364868df634deff
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-26 12:15:19 +0000

    fix bugs and organise code

--- 28_ch6_ps/script.js
+++ 28_ch6_ps/script.js
@@ -1,18 +1,19 @@
-let age = prompt("Enter your age");
-age = Number.parseInt(age);
 let runAgain = true;
 
+const canDrive = (age) => {
+  return age>=18?true:false;
+}
+
 while (runAgain){
-  const canDrive = (age) => {
-    return age>=18?true:false;
-  }
-  
+  let age = prompt("Enter your age");
+  age = Number.parseInt(age);
   if (canDrive(age)){
     alert("Yes you can drive")
   }
   else{
     alert("You cannot drive")
   }
+  runAgain = confirm("Do you want to play again?")
 }
 
 
