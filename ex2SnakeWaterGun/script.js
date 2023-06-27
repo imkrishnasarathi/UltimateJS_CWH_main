f3896354fb15b0614bfba131974356e0f47a83d6
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-27 12:46:51 +0000

    edit script.js

--- ex2SnakeWaterGun/script.js
+++ ex2SnakeWaterGun/script.js
@@ -1,9 +1,25 @@
 const choices = ["s", "w", "g"];
 const readyOrNot = confirm("Hello there! Are you ready for this game ?");
+let cScore;
+let uScore;
 
 function Game(){
-  let item = items[Math.floor(Math.random()*items.length)];
-  let userChoice = prompt("Enter your choice (rock, paper or scissors) :");
+  let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+  let count = 0;
+  while (count<6){
+    let userChoice = prompt("Enter your choice (snake, water or gun) :");
+    if (userChoice === computerChoice){
+      alert("It's a draw!");
+      uScore++;
+      cScore++;
+    }
+    if (userChoice === 'snake' || userChoice === 'Snake'){
+      if (computerChoice === 'w'){
+        alert("You Won! Snake Drinks Water!")
+      }
+    } 
+  }
+  
 }
 
 if (readyOrNot){
