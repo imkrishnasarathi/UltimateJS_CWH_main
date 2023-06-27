6637e5987ee590bc66927d9db47a144e8d4afc83
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-27 12:51:25 +0000

    code the game

--- ex2SnakeWaterGun/script.js
+++ ex2SnakeWaterGun/script.js
@@ -1,23 +1,33 @@
-const choices = ["s", "w", "g"];
+const choices = ["snake", "water", "gun"];
 const readyOrNot = confirm("Hello there! Are you ready for this game ?");
 let cScore;
 let uScore;
 
 function Game(){
-  let computerChoice = choices[Math.floor(Math.random()*choices.length)];
   let count = 0;
   while (count<6){
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
     let userChoice = prompt("Enter your choice (snake, water or gun) :");
-    if (userChoice === computerChoice){
+    if (userChoice.toLowerCase() === computerChoice){
       alert("It's a draw!");
       uScore++;
       cScore++;
     }
-    if (userChoice === 'snake' || userChoice === 'Snake'){
-      if (computerChoice === 'w'){
-        alert("You Won! Snake Drinks Water!")
+    if (userChoice.toLowerCase() === 'snake'{
+      if (computerChoice === 'water'){
+        alert("You Won! Snake Drinks Water!");
+        uScore++;
       }
-    } 
+      else if (computerChoice === 'gun'){
+        alert("You Lost! Gun shoots Snake!")
+        cScore++;
+      }
+    }
+    else if (userChoice.toLowerCase() === 'water'){
+      if (computerChoice === 'snake'){
+        alert("You Lost! Snake drinks water!");
+      }
+    }
   }
   
 }
