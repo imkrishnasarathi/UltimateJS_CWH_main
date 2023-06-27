2470b6da35b892bbbcea1125f467f1210133aa3f
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-27 12:40:59 +0000

    create a function game and get a random choice from the computer

--- ex2SnakeWaterGun/script.js
+++ ex2SnakeWaterGun/script.js
@@ -1,7 +1,13 @@
 const choices = ["s", "w", "g"];
 const readyOrNot = confirm("Hello there! Are you ready for this game ?");
+
+function Game(){
+  let item = items[Math.floor(Math.random()*items.length)];
+}
+
 if (readyOrNot){
   alert("Let's Go!");
+  
 }
 else{
   alert("Okay. Maybe we'll play later ?");
