7f1154e2a81369def22a3da751f498eb2a7aeba1
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-27 12:32:01 +0000

    Check if the user wants to play and run accordingly

--- ex2SnakeWaterGun/script.js
+++ ex2SnakeWaterGun/script.js
@@ -1,1 +1,8 @@
 const choices = ["s", "w", "g"];
+const readyOrNot = confirm("Hello there! Are you ready for this game ?")
+if (readyOrNot){
+  alert("Let's Go!")
+}
+else{
+  alert("Okay. Maybe we'll play later ?")
+}
