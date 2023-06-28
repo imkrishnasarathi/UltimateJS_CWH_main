24f55bb7296bf47a354d638342d4ef1e67f939e7
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-06-28 13:11:35 +0000

    move code block

--- ex2SnakeWaterGun/script.js
+++ ex2SnakeWaterGun/script.js
@@ -3,74 +3,73 @@ const readyOrNot = confirm("Hello there! Are you ready for this game ?");
 let cScore;
 let uScore;
 
-function Game(){
+function Game() {
   let count = 0;
-  while (count<6){
-    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+  while (count < 6) {
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     let userChoice = prompt("Enter your choice (snake, water or gun) :");
-    if (userChoice.toLowerCase() === computerChoice){
+    if (userChoice.toLowerCase() === computerChoice) {
       alert("It's a draw!");
       uScore++;
       cScore++;
       count++;
     }
-    if (userChoice.toLowerCase() === 'snake'){
-      if (computerChoice === 'water'){
+    if (userChoice.toLowerCase() === 'snake') {
+      if (computerChoice === 'water') {
         alert("You Won! Snake Drinks Water!");
         uScore++;
       }
-      else if (computerChoice === 'gun'){
+      else if (computerChoice === 'gun') {
         alert("You Lost! Gun shoots Snake!");
         cScore++;
       }
       count++;
     }
-    else if (userChoice.toLowerCase() === 'water'){
-      if (computerChoice === 'snake'){
+    else if (userChoice.toLowerCase() === 'water') {
+      if (computerChoice === 'snake') {
         alert("You Lost! Snake drinks water!");
         cScore++;
       }
-      else if (computerChoice === 'gun'){
+      else if (computerChoice === 'gun') {
         alert("You Won! Water drowns Gun!");
         uScore++;
       }
       count++;
     }
-    else if (userChoice.toLowerCase() === "gun"){
-      if (computerChoice === "water"){
+    else if (userChoice.toLowerCase() === "gun") {
+      if (computerChoice === "water") {
         alert("You Lost! Water drowns Gun!");
         cScore++;
       }
-      else if (computerChoice === "snake"){
+      else if (computerChoice === "snake") {
         alert("You Won! Gun shoots Snake!");
         uScore++;
       }
       count++;
     }
-    else{
+    else {
       alert("Not a valid word. Maybe try again?");
       continue;
     }
   }
-  
 }
 
-if (readyOrNot){
+if (readyOrNot) {
   alert("Let's Go!");
   Game();
-  if (cScore > uScore){
+  if (cScore > uScore) {
     alert("The Winner is COMPUTER!");
     document.write("The Winner is COMPUTER!");
   }
-  else if (uScore > cScore){
+  else if (uScore > cScore) {
     alert("Hooray! You are the winner!");
     document.write("Hooray! You are the winner!");
   }
-  else if (uScore === cScore){
+  else if (uScore === cScore) {
     alert("It's a draw");
     document.write("It's a draw");
   }
 }
-else{
+else {
   alert("Okay. Maybe we'll play later ?");
 }
