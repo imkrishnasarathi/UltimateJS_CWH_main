d55c1aa02f1b6f65efac7f86165044c54790e099
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-07-13 01:40:54 +0000

    log 'a' variable's first child's next sibling

--- 33_parents_siblings/script.js
+++ 33_parents_siblings/script.js
@@ -2,4 +2,4 @@ console.log(document.body.firstChild);
 a = document.body.firstChild;
 console.log(a.parentNode);
 console.log(a.parentElement);
-console.log(a.firstChild);
+console.log(a.firstChild.nextSibling);
