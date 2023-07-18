8fc383d715a62d850984fe4bb1188cdd764e852b
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-07-18 01:26:34 +0000

    comment out current code

--- 36_SearchingDom/script.js
+++ 36_SearchingDom/script.js
@@ -1,3 +1,4 @@
 // Change the card title to red
-let ctitle = document.getElementsByClassName('card-title')[0];
-ctitle.style.color = "red"
+/* let ctitle = document.getElementsByClassName('card-title')[0];
+   ctitle.style.color = "red" */
+
