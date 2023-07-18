a38bff257d756d0a0f24313cf0f4a15cb35842f3
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-07-18 01:37:22 +0000

    change color of all the card titles using js

--- 36_SearchingDom/script.js
+++ 36_SearchingDom/script.js
@@ -6,3 +6,6 @@ let ctitle = document.getElementById("firstcardtitle")
 ctitle.style.color = "blue"
 
 let ctitles = document.querySelectorAll('.card-title')
+ctitles[0].style.color = "blue"
+ctitles[1].style.color = "red"
+ctitles[2].style.color = "green"
