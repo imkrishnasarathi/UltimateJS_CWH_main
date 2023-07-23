3ebab13190f01261ff57ebd73d73661c79a22b8d
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-07-23 07:03:15 +0000

    update video 41 files

--- 41_innerHTML/script.js
+++ 41_innerHTML/script.js
@@ -1,2 +1,13 @@
-console.log(document.body.firstChild.nodeName)
-console.log(document.body.firstElementChild.nodeName)
+// let x = document.getElementsByTagName('span')[0]
+// console.log(x)
+
+// let y = document.getElementsByTagName('span')[0]
+// console.dir(x)
+
+// console.log(document.body.firstChild.nodeName)
+// console.log(document.body.firstElementChild.nodeName)
+
+// first.outerHTML
+// => '<span id="first"><b>Hi</b>Hey I am Span</span>'
+// first.outerHTML = "<div>Hey</div>"
+// => '<div>Hey</div>'
