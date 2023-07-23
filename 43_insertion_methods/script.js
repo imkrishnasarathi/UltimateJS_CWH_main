67aecb996c07b57193cc735d390ff16a0973a7ea
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-07-23 13:31:57 +0530

    update video 43 files

--- 43_insertion_methods/script.js
+++ 43_insertion_methods/script.js
@@ -1,3 +1,5 @@
 let a = document.getElementsByTagName('div')[0]
 
-a.innerHTML = a.innerHTML + "<h1>Hello World</h1>"
+// a.innerHTML = a.innerHTML + "<h1>Hello World</h1>"
+
+let div = document.createElement('div');
