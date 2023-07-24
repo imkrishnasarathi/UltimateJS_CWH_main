5f82a6c9fa0d7ce557470125b39cca3f79d7f5fa
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-07-24 13:27:01 +0530

    update video 46 files

--- 46_tut/script.js
+++ 46_tut/script.js
@@ -1,14 +1,18 @@
 alert("hello");
 
-let a = setTimeout(function() {
-    alert("I am inside settimeout")
-}, 1000);
+const sum = (a, b) => {
+    console.log("yes I am running")
+    a+b}
 
-let b = confirm("Do you want to run the settimeout?")
+// let a = setTimeout(function() {
+//     alert("I am inside settimeout")
+// }, 1000);
+
+// let b = confirm("Do you want to run the settimeout?")
 
 
-if(b === false){
-    clearTimeout(a);
-}
+// if(b === false){
+//     clearTimeout(a);
+// }
 
-console.log(a)
+// console.log(a)
