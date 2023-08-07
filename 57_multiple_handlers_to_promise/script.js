18a360ccad3dafe27a373620d69489907f25f5d9
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-07 18:37:19 +0530

    update

--- 57_multiple_handlers_to_promise/script.js
+++ 57_multiple_handlers_to_promise/script.js
@@ -1,6 +1,10 @@
 let p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        alert("Hey I am resolved")
+        alert("Hey I am not resolved")
         resolve(1)
     }, 2000)
+})
+
+p1.then(()=>{
+    console.log('Congratulations this promise is now resolved')
 })
